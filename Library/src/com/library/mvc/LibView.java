465e0732b251114fb4dbfabaa0b259bbe8d8db2f
Jenelle Michael <jenmike@mmicorp.metmom.mmih.biz>
2020-03-18 09:06:21 +0200

    Functional, but still case sensitive

--- Library/src/com/library/mvc/LibView.java
+++
@@ -1,8 +0,0 @@
-package com.library.mvc;
-
-class LibView{
-    protected void printLibrary(String userName, String userArgs){
-        System.out.println("Hi, " + userName + ":)");
-        System.out.println();
-    }
-}
